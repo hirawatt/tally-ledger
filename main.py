2e9947995a0126539d1bd0cc93aa9d9f12b0251a
VH <hirawatt@pm.me>
2023-08-10 19:53:13 +0530

    minor code refactor

--- main.py
+++ main.py
@@ -18,27 +18,26 @@ def xml_to_df(root):
     df = pd.DataFrame(data)
     return df
 
-st.set_page_config("Ledger to Tally Prime XML", "📒", "wide")
-# INPUTS
-st.sidebar.title("Ledger to Tally Prime XML")
-ledger = st.sidebar.file_uploader("Upload Ledger Contract Note", type=["xlsx"])
-col1, col2 = st.columns(2)
-date = col1.date_input("Select Date")
-output_type = col2.radio("Select output type", options=["Business", "Investment"])
-
 # TODO: add data validation logic
 def data_validation():
     return True
 
+st.set_page_config("Ledger to Tally Prime XML", "📒", "wide")
+st.sidebar.title("Ledger to Tally Prime XML")
+# INPUTS
+col1, col2 = st.columns(2)
+date = col1.date_input("Select Date")
+output_type = col2.radio("Select output type", options=["Business", "Investment"])
+
 st.info("Testing")
+ledger = st.sidebar.file_uploader("Upload Ledger Contract Note", type=["xlsx"])
 # Read uploaded xlsx file
 if ledger is not None:
     df = pd.read_excel(ledger, sheet_name='Sheet1', header=None)
     st.dataframe(df, use_container_width=True)
 
-st.sidebar.subheader("XML Testing")
+st.sidebar.info("XML Testing")
 xml_file = st.sidebar.file_uploader("Upload Exported Tally XML", type=["xml"])
-
 # Read uploaded xml file
 if xml_file is not None:
     xml_file_read = parse_xml(xml_file)
@@ -51,16 +50,18 @@ if xml_file is not None:
     c1.write(new_df2)
 
 # Testing
-with open('data-new/nse_janmangal.xml', 'rb') as file1:
+#with open('data-new/nse_janmangal.xml', 'rb') as file1:
+with open('output/sample3.xml', 'rb') as file1:
     f1 = file1.read()
-    f1_str = f1.decode('utf-16')
+    #f1_str = f1.decode('utf-16')
+    f1_str = f1.decode('utf-8')
 
 # Create XML file
 doc, tag, text, line = Doc().ttl()
 
 with tag('ENVELOPE'):
-    # 12 entries for each trade
     doc.asis('<!--HirawatTech watermark-->')
+    # 12 entries for each trade
     # TODO: add date/year from excel
     line('DSPVCHDATE', '7-Jul-23')
     line('DSPVCHLEDACCOUNT', 'Stock')
@@ -75,16 +76,19 @@ with tag('ENVELOPE'):
     line('DSPVCHCRAMT', 'placeholder')
     line('DSPEXPLVCHNUMBER', 'placeholder')
     doc.asis('<!--testing-->')
-    # business/investment logic
-    if output_type == "Business":
-        # TODO: add Sale/Purchase logic
-        line('DSPVCHEXPLACCOUNT', 'Sale of Shares')
-    elif output_type == "Invetment":
-        line('DSPVCHEXPLACCOUNT', 'Invetment in Shares')
-    # TODO: add for loop based on number of excel entries
-    with tag('DSPVCHEXPLVALUE'):
-        line('DSPVCHEXPLDRAMTEXCELEXPORT', '10')
-        line('DSPVCHEXPLCRAMTEXCELEXPORT', '')
+    # FIXME: add number of entries logic
+    no_of_entries = 3
+    for i in range(0, no_of_entries):
+        # business/investment logic
+        if output_type == "Business":
+            # TODO: add Sale/Purchase logic
+            line('DSPVCHEXPLACCOUNT', 'Sale of Shares')
+        elif output_type == "Invetment":
+            line('DSPVCHEXPLACCOUNT', 'Invetment in Shares')
+        # TODO: add for loop based on number of excel entries
+        with tag('DSPVCHEXPLVALUE'):
+            line('DSPVCHEXPLDRAMTEXCELEXPORT', '10')
+            line('DSPVCHEXPLCRAMTEXCELEXPORT', '')
 
 f2_str = indent(doc.getvalue())
 
