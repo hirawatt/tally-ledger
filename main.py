2f6c26819f6fd2dfb246974b5dbdc51b93145d05
VH <hirawatt@pm.me>
2023-08-12 12:30:15 +0530

    UI refactor + removed testing features

--- main.py
+++ main.py
@@ -24,14 +24,16 @@ def xml_to_df(root):
 def data_validation():
     return True
 
+# Testing Rule
+testing = False
+
 st.set_page_config("Ledger to Tally Prime XML", "📒", "wide")
 st.sidebar.title("Ledger to Tally Prime XML")
 # INPUTS
 col1, col2 = st.columns(2)
-date = col1.date_input("Select Date")
+date_input = col1.date_input("Select Date")
 output_type = col2.radio("Select output type", options=["Business", "Investment"])
 
-st.info("Testing")
 ledger = st.sidebar.file_uploader("Upload Ledger Contract Note", type=["xlsx"])
 # Read uploaded xlsx file
 if ledger is not None:
@@ -50,61 +52,67 @@ if ledger is not None:
     # Column 1 of excel sheet
     df_subset_c1 = df_subset.loc[:, 1].dropna()
     df_subset_c4 = df_subset.loc[:, 4].dropna()
-    #st.write(df_subset_c1)
-    st.write(df_subset_c4)
-    # journal entry
-    df_subset_c1_j = df_subset_c1[df_subset_c1.str.startswith("J")]
-    df_journal = df.iloc[df_subset_c1_j.index]
-    st.write(df_journal)
     # bank entry
     df_subset_c1_b = df_subset_c1[df_subset_c1.str.startswith("B")]
     df_bank = df.iloc[df_subset_c1_b.index]
-    st.write(df_bank)
+    # journal entry
+    df_subset_c1_j = df_subset_c1[df_subset_c1.str.startswith("J")]
+    df_journal = df.iloc[df_subset_c1_j.index]
     # trades entry
     df_subset_c1_date = df_subset_c1[df_subset_c1.str.contains("/")]
     df_trades = df.iloc[df_subset_c1_date.index]
-    st.write(df_trades)
     # FIXME: edge case: 2 trades with summed up charges | trades entry alternative
     df_subset_c4_trades = df_subset_c4[df_subset_c4.str.contains("Dl", case=True)]
-    st.write(df.iloc[df_subset_c4_trades.index])
+    # Output
+    if testing:
+        st.info("Testing")
+        #st.write(df_subset_c1)
+        st.write(df_subset_c4)
+        st.write(df_bank)
+        st.write(df_journal)
+        st.write(df_trades)
+        st.write(df.iloc[df_subset_c4_trades.index])
 
-    # Data Processing
-    # FIXME: add number of entries logic
-    no_of_trades = df_trades.shape[0]
+    # Data Processing - number of entries logic
     no_of_bank_entries = df_bank.shape[0]
-    st.write(df_bank[11].iloc[0])
+    no_of_journal_entries = df_journal.shape[0]
+    no_of_trades = df_trades.shape[0]
+    #st.write(df_journal[11].iloc[0])
 
-st.sidebar.info("XML Testing")
-xml_file = st.sidebar.file_uploader("Upload Exported Tally XML", type=["xml"])
-# Read uploaded xml file
-if xml_file is not None:
-    xml_file_read = parse_xml(xml_file)
-    df = xml_to_df(xml_file_read)
-    c1, c2 = st.columns(2)
-    new_df1 = df[df.iloc[:,0].notna()]
-    new_df2 = df[df.iloc[:,1].notna()]
-    c2.write(new_df1)
-    c1.write(new_df2)
+if testing:
+    st.info("Testing")
+    st.sidebar.info("XML Testing")
+    xml_file = st.sidebar.file_uploader("Upload Exported Tally XML", type=["xml"])
+    # Read uploaded xml file
+    if xml_file is not None:
+        xml_file_read = parse_xml(xml_file)
+        df = xml_to_df(xml_file_read)
+        c1, c2 = st.columns(2)
+        new_df1 = df[df.iloc[:,0].notna()]
+        new_df2 = df[df.iloc[:,1].notna()]
+        c2.write(new_df1)
+        c1.write(new_df2)
 
-# FIXME: Testing - remove when complete
-with open('data-new/nse_janmangal.xml', 'rb') as file1:
-#with open('output/sample3.xml', 'rb') as file1:
-    f1 = file1.read()
-    f1_str = f1.decode('utf-16')
-    #f1_str = f1.decode('utf-8')
+    # FIXME: Testing - remove when complete
+    with open('data-new/nse_janmangal.xml', 'rb') as file1:
+    #with open('output/sample3.xml', 'rb') as file1:
+        f1 = file1.read()
+        f1_str = f1.decode('utf-16')
+        #f1_str = f1.decode('utf-8')
 
 # FIXME: debit/credit/balance logic
-def check_dcb(df_row):
+def check_dcb(df_row, i):
     # row 9 - debit, row 10 - credit, row 11 - balance
-    if df_row[9] != "nan":
-        value = df_row[9]
-    elif df_row[10] != "nan":
-        value = df_row[10]
+    if df_row[9].iloc[i] != "nan":
+        value = df_row[9].iloc[i]
+    elif df_row[10].iloc[i] != "nan":
+        value = df_row[10].iloc[i]
     else:
-        value = df_row[11]
+        value = df_row[11].iloc[i]
     return value
 
 def create_xml_from_ledger():
+    print(f"------------------ {date_input} ----------------------")
     # Create XML file
     doc, tag, text, line = Doc().ttl()
     doc.asis('<!--HirawatTech watermark-->')
@@ -113,7 +121,7 @@ def create_xml_from_ledger():
         for i in range(0, no_of_bank_entries):
             date = df_bank[0].iloc[i].strftime('%d-%b-%y')
             tick = df_bank[4].iloc[i]
-            amount = df_bank[9].iloc[i]
+            amount = check_dcb(df_bank, i) #df_bank[9].iloc[i]
             print(f"Bank Data {i}: ", date, tick, amount)
             # 12 Tally XML entries for bank entry
             # TODO: add date/year from excel
@@ -129,6 +137,13 @@ def create_xml_from_ledger():
             line('DSPVCHDRAMT', '')
             line('DSPVCHCRAMT', f'{amount}')
             line('DSPEXPLVCHNUMBER', 'placeholder')
+
+        # FIXME: update Journal Entry
+        doc.asis('<!--Journal Entry-->')
+        for i in range(0, no_of_journal_entries):
+            date = df_journal[0].iloc[i].strftime('%d-%b-%y')
+            amount = df_journal[10].iloc[i]
+            print(f"Journal Data {i}: ", date, amount)
 
         doc.asis('<!--Trades Entry-->')
         for i in range(0, no_of_trades):
@@ -154,7 +169,7 @@ def create_xml_from_ledger():
             line('DSPVCHSTOCKITEM', f'{tick}')
             line('DSPVCHBILLEDQTY', f'{abs(shares)} qnty')
             line('DSPVCHRATE', f'{price}/qnty')
-            line('DSPVCHVALUE', f'{amount}')
+            line('DSPVCHVALUE', f'{-amount}')
             # FIXME: STT, GST & other charges logic
             line('DSPVCHEXPLACCOUNT', 'S T T')
             with tag('DSPVCHEXPLVALUE'):
@@ -169,16 +184,17 @@ def create_xml_from_ledger():
                 line('DSPVCHEXPLDRAMTEXCELEXPORT', f'{amount}')
                 line('DSPVCHEXPLCRAMTEXCELEXPORT', '')
             line('DSPEXPLVCHNUMBER', '')
-            # FIXME: update Journal Entry
     return indent(doc.getvalue())
 
 data_valid = data_validation()
 if data_valid:
     f2_str = create_xml_from_ledger()
     st.download_button('Download Tally XML', f2_str, 'download.xml', 'application/xml', use_container_width=True)
-    # Display comparison
-    co1, co2 = st.columns(2)
-    co1.code(f1_str, language="xml")
-    co2.code(f2_str, language="xml")
+    with st.expander("Tally XML Display"):
+        # Display comparison
+        co1, co2 = st.columns(2)
+        #co1.code(f1_str, language="xml")
+        #co2.code(f2_str, language="xml")
+        st.code(f2_str, language="xml")
 else:
     st.warning(data_valid)
